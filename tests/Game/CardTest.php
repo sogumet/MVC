a0e2774c8b5e54d836633d8029fad57f4924c685
Kent Sjöberg <sogumet@gmail.com>
2022-04-23 21:23:14 +0100

    Deck Class Test completed

--- tests/Game/CardTest.php
+++ tests/Game/CardTest.php
@@ -51,36 +51,4 @@ class CardTest extends TestCase
         $exp = "img/deck/2-clubs.png";
         $this->assertEquals($exp, $res);
     }
-
-    /**
-     * Create object and testing cardCount function
-     */
-    /* public function testCardCount() {
-        $hand = new Hand();
-        $this->assertInstanceOf("\App\Deck\Hand", $hand);
-        $deck = new Deck();
-        $this->assertInstanceOf("\App\Deck\Deck", $deck);
-        $cards =  $deck->drawCards(10);
-        $hand->addCards($cards);
-        $res = $hand->cardCount();
-        $exp = 11;
-        $this->assertEquals($exp, $res);   
-    }
- */
-    /**
-     * Create object and testing addCard function
-     */
-    /* public function testAddCard()
-    {
-        $hand = new Hand();
-        $this->assertInstanceOf("\App\Deck\Hand", $hand);
-        $deck = new Deck();
-        $this->assertInstanceOf("\App\Deck\Deck", $deck);
-        $card =  $deck->drawCards(0);
-        $hand->addCard($card[0]);
-        $exp = $card[0];
-        $res = $hand->hand[0];
-        $this->assertEquals($exp, $res);   
-    } */
-
 }
---
+++ tests/Game/DeckTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Deck;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
+
+/**
+ * Test cases for class Guess.
+ */
+class DeckTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testCreateDeckObject()
+    {
+        $deck = new Deck();
+        $this->assertInstanceOf("\App\Deck\Deck", $deck); 
+    }
+
+    /**
+     * Create object and testing drawCard function
+     */
+    public function testDrawCard()
+    {
+        $deck = new Deck();
+        $this->assertInstanceOf("\App\Deck\Deck", $deck);
+        $exp = $deck->deck[0];
+        $res = $deck->drawCard();
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Create object and testing cardCount function
+     */
+    public function testCardCount()
+    {
+        $deck = new Deck();
+        $this->assertInstanceOf("\App\Deck\Deck", $deck);
+        $deck->drawCards(9);
+        $exp = 42;
+        $res = $deck->cardCount();
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Create object and testing shuffleDeck function
+     */
+    public function testShuffleDeck()
+    {
+        $deck = new Deck();
+        $this->assertInstanceOf("\App\Deck\Deck", $deck);
+        $deck1 = new Deck();
+        $deck->shuffleDeck();
+        $res = $deck->deck;
+        $exp = $deck1->deck;
+        $this->assertSamesize($exp, $res);
+    }
+
+}
