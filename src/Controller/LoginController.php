6d4a29a181ea759d161eb477da43ca4a4c475462
Kent Sjöberg <sogumet@gmail.com>
2022-05-23 13:45:34 +0100

    Final version

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -34,4 +34,14 @@ class LoginController extends AbstractController
         $email = $user->getEmail();
         return $this->redirectToRoute('user', ['email' => $email]);
     }
+
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
